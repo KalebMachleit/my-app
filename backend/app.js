4528cfdb6361a9e18d4727e13e4cea662523d366
Kaleb Machleit <machleit0225@gmail.com>
2023-06-28 22:24:18 -0700

    no styling for now but I just need this done

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/home', (req, res)=> {
+    fs.readFile('../frontend/home.html', (err, data)=> {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+});
+
+app.use('/about', (req, res)=> {
+    fs.readFile('../frontend/about.html', (err, data)=> {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+});
+
+app.use('/uhoh', (req, res)=> {
+    res.status(404).send("Oh uh, something went wrong");
+})
+
+app.use('/contact', (req, res)=> {
+    fs.readFile('../frontend/contact.html', (err, data)=> {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on PORT: ${PORT}`);
+})
